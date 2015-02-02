410df6b43d998057a8665d56ea3345a4476e0356
Allan R. <careitan2000@yahoo.com>
2015-02-02 00:40:22 -0800

    First Intro

--- HW2/reitanc.adventure.c
+++ HW2/reitanc.adventure.c
@@ -3,36 +3,46 @@
 */
 
 #include <stdio.h>
-#include <iostream.h>
 #include <string.h>
-#include <cstring.h>
-#include <cstdlib.h>
-#include <vector.h>
-#include <iterator.h>
-using namespace std;
+#include <stdlib.h>
 
 // Function definitions
 void Introduction();
+int RoomConnections();
 
 // Struct definition
-struct Room()
-{
-
+struct Room {
+  char roomName[16];
+  char connection[6][16];
+  char roomType[12];
 };
 
 main()
 {
+  //  Initialize random seed
+  srand(1000);
+
   // Initialize variables
-  vector<string> travel; // used to keep track of the steps that were taken.
+
+  // Generate the game content.
+
+  // Begin the Game for the user.
+  Introduction();
 
 
 
+  return 0;
+}
+
+// Function Build outs
+void Introduction()
+{
+  printf("Welcome to the land of CS 344 Adventure.\n\n");
 
   return;
 }
 
-void Introduction()
+int RoomConnections()
 {
-
-
-}
+  return (rand() % 4) + 3;
+};
