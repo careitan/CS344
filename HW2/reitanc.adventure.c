2ba7195a8ccd406b6376d161d08cb29963db6c9e
Allan R. <careitan2000@yahoo.com>
2015-02-01 23:00:13 -0800

    Renamed file
    Renamed for naming conventions and guidelines.

---
+++ HW2/reitanc.adventure.c
@@ -0,0 +1,31 @@
+/* 
+* Allan Reitan CS 344 - Homework 2 Adventure Game
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// Function definitions
+void Introduction();
+
+// Struct definition
+struct Room()
+{
+  
+};
+
+main()
+{
+  /* code */
+
+
+
+
+  return;
+}
+
+void Introduction()
+{
+
+
+}
