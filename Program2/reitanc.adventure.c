f904a81f4b5e9744d9517bb8cfd2a08c6468c1bb
Craig Reitan <reitanc@os1.engr.oregonstate.edu>
2017-10-14 17:44:53 -0700

    Starting Program 2 to build the Adventure game.

---
+++ Program2/reitanc.adventure.c
@@ -0,0 +1,17 @@
+/******
+ * OSU Fall 2017 CS 344 - Program 2
+ * (Craig) Allan Reitan
+ * Adventure File for program implementation
+ * 10/14/2017
+ * ****/
+
+#include "stdio.h"
+
+
+void main(int argc, char* argv[])
+{
+
+
+
+return
+}
