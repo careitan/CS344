a230dcc8247e56f4487cef917c98bb4d6fdef0f8
Craig Reitan <reitanc@os1.engr.oregonstate.edu>
2017-10-28 20:19:55 -0700

    Initial break out of file.

---
+++ Program3/smallsh.c
@@ -0,0 +1,36 @@
+/*******************************
+* OSU CS344 - Fall Quarter 2017
+* Program 3 - Small Shell Project
+* (Craig) Allan Reitan
+* 10-28-2017
+* 
+* Assignment is to create a shell engine in C
+*
+********************************/
+#include <assert.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <math.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+typedef int bool;
+#define true  1
+#define false 0
+
+
+
+
+
+void main()
+{
+
+
+
+}
+
